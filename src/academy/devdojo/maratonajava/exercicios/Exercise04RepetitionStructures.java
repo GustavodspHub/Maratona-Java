275ced5cdf6f17a23031e407c82ac0d92c774c81
Gustavo Pereira <gustavopereira0979@gmail.com>
2024-03-11 17:43:09 -0300

    feat: Exercises 04 about repetition structures

---
+++ src/academy/devdojo/maratonajava/exercicios/Exercise04RepetitionStructures.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.exercicios;
+// Imprima todos os números pares de 0 até 1000000
+
+public class Exercise04RepetitionStructures {
+    public static void main(String[] args) {
+        for(int index = 0; index <= 1000000; ++index) {
+            if(index % 2 == 0) System.out.println(index);
+        }
+    }
+}
