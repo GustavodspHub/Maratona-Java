f2829c3828f592fa3f5d7b0925481efea78d3e24
Gustavo Pereira <gustavopereira0979@gmail.com>
2024-06-04 23:47:27 -0300

    feat: Override

---
+++ src/academy/devdojo/maratonajava/OOP/sobrescrita/test/TestSobrescrita.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.OOP.sobrescrita.test;
+
+import academy.devdojo.maratonajava.OOP.sobrescrita.domain.Sobrescrita;
+
+public class TestSobrescrita {
+    public static void main(String [] args) {
+        Sobrescrita sobrescrita = new Sobrescrita();
+        System.out.println(sobrescrita);
+    }
+}
