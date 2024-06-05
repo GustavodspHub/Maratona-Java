9fd14c393c4a0b953a55ce09f5d0af4076f51995
Gustavo Pereira <gustavopereira0979@gmail.com>
2024-06-04 23:47:43 -0300

    feat: Override

---
+++ src/academy/devdojo/maratonajava/OOP/sobrescrita/domain/Sobrescrita.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.OOP.sobrescrita.domain;
+
+public class Sobrescrita {
+    @Override
+    public String toString() {
+        return "I'm returning something different";
+    }
+}
